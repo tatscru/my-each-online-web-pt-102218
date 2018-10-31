d1ff5246f16047d8507b5cd04ea37f73568e0594
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-31 00:23:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  i = 0 
+  while i < array.length 
+  # it is iterating the amount of items within an array- whatever is it = .length
+    yield array[i]
+  # call/grab, pass through, each element within the array 
+    i = i + 1
+  # it is now repeating over each element within an array 
+ end
+  array
+  # does not modify the original array, that is why we have to call it again-- not modifying it by each 
+end 
