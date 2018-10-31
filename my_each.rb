24ac2c78a006b8b7b360ab97c34a11bbb15f134f
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-31 00:21:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,7 @@ while i < array.length
   # it is iterating the amount of items within an array- whatever is it = .length
     yield array[i]
   # call/grab, pass through, each element within the array 
-  i = i + 1
+    i = i + 1
   it is now repeating over each element within an array 
  end
   # array
