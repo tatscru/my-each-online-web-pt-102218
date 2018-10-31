b29e29c80d049d3d40069e92e657f052eb6db956
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-31 00:22:23 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -5,7 +5,7 @@ def my_each(array)
     yield array[i]
   # call/grab, pass through, each element within the array 
     i = i + 1
-  it is now repeating over each element within an array 
+  # it is now repeating over each element within an array 
  end
   array
 end 
