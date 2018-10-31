8f30519d497c15f791b7e3255b5aabee3d8245f1
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-31 00:20:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array)
+  i = 0 
+while i < array.length 
+  # it is iterating the amount of items within an array- whatever is it = .length
+    yield array[i]
+  # call/grab, pass through, each element within the array 
+  i = i + 1
+  it is now repeating over each element within an array 
+ end
+  # array
+end 
